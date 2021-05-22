904e9a3933185d11d05807e2c003395fc320085d
Anton Vovk <Anton.Vovk@vig.pl>
2021-05-22 09:37:03 +0300

    Initial project setup

---
+++ src/main/kotlin/com/antonvovk/ThyroidNoduleDMS/ThyroidNoduleDmsApplication.kt
@@ -0,0 +1,11 @@
+package com.antonvovk.ThyroidNoduleDMS
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThyroidNoduleDmsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ThyroidNoduleDmsApplication>(*args)
+}
