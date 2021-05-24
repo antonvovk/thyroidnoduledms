ce1274b36f29df77b0e62b147d7a6b0785c434a2
Anton Vovk <Anton.Vovk@vig.pl>
2021-05-24 20:55:00 +0300

    Added QuestionAnswer.kt

---
+++ src/main/kotlin/com/antonvovk/ThyroidNoduleDMS/db/testing/models/QuestionAnswer.kt
@@ -0,0 +1,20 @@
+package com.antonvovk.ThyroidNoduleDMS.db.testing.models
+
+import com.antonvovk.ThyroidNoduleDMS.db.models.BaseAuditEntity
+import javax.persistence.*
+
+@Entity
+@Table(name = "QuestionAnswer", schema = "testing")
+data class QuestionAnswer(
+
+    @Column(name = "answerText")
+    var answerText: String
+) : BaseAuditEntity() {
+
+    @OneToMany
+    private val answeredQuestions: List<AnsweredQuestion> = emptyList()
+
+    @OneToOne
+    @JoinColumn(name = "testingQuestionId")
+    lateinit var testingQuestion: TestingQuestion
+}
